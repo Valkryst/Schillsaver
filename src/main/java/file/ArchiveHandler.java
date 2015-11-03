1ce6ecdae5f142fbfbe48c908c85a167dc9e4cc1
Valkryst <valkryst@valkryst.com>
2015-11-03 14:46:44 -0400

    Added documentation to the two methods in the class.

--- src/main/java/file/ArchiveHandler.java
+++ src/main/java/file/ArchiveHandler.java
@@ -8,6 +8,18 @@ import java.util.Formatter;
 import java.util.Locale;
 
 public class ArchiveHandler {
+    /**
+     * Compresses the specified file while outputting the command-line
+     * results to the screen.
+     *
+     * The resulting archive will bear the name of the input file.
+     * If the input file is test.jpg, then the output archive will be test.jpg.7z
+     * as an example.
+     * @param selectedFile The file to compress.
+     * @param controller The controller for the view in which the output text area resides.
+     * @param configHandler The object that handles settings for encoding, decoding, compression, and a number of other features.
+     * @return The compressed archive.
+     */
     public File packFile(final File selectedFile, final MainScreenController controller,  final ConfigHandler configHandler) {
         // Basic command settings ripped from http://superuser.com/a/742034
         final StringBuilder stringBuilder = new StringBuilder();
@@ -35,6 +47,17 @@ public class ArchiveHandler {
         return file;
     }
 
+    /**
+     * Compresses the specified file(s), while outputting the command-line
+     * results to the screen, into a single archive.
+     *
+     * The resulting archive will bear the specified name.
+     * @param selectedFiles The files to compress.
+     * @param controller The controller for the view in which the output text area resides.
+     * @param configHandler The object that handles settings for encoding, decoding, compression, and a number of other features.
+     * @param archiveName The name to give the compressed archive.
+     * @return The compressed archive.
+     */
     public File packFiles(final File[] selectedFiles, final MainScreenController controller,  final ConfigHandler configHandler, final String archiveName) {
         // Basic command settings ripped from http://superuser.com/a/742034
         final StringBuilder stringBuilder = new StringBuilder();
