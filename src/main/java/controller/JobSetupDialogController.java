a253a24adc32a86fdceeb3b9c8ffee0e2b57789e
Valkryst <valkryst@valkryst.com>
2015-11-07 17:29:18 -0400

    Initial commit.

---
+++ src/main/java/controller/JobSetupDialogController.java
@@ -0,0 +1,237 @@
+package controller;
+
+import handler.ConfigHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import misc.Job;
+import model.JobSetupDialogModel;
+import view.JobSetupDialogView;
+
+import javax.swing.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class JobSetupDialogController extends Stage implements EventHandler {
+    // todo JavaDoc
+    private final JobSetupDialogView view;
+    // todo JavaDoc
+    private final JobSetupDialogModel model;
+
+    /** The object that handles settings for encoding, decoding, compression, and a number of other features. */
+    private final ConfigHandler configHandler;
+
+    /**
+     * Construct a new job setup dialog controller.
+     * @param configHandler The object that handles settings for encoding, decoding, compression, and a number of other features.
+     */
+    public JobSetupDialogController(final Stage primaryStage, final ConfigHandler configHandler) {
+        this.configHandler = configHandler;
+
+        view = new JobSetupDialogView(primaryStage, this);
+        model = new JobSetupDialogModel();
+
+        // Setup Stage:
+        final Scene scene = new Scene(view);
+        scene.getStylesheets().add("global.css");
+        scene.getRoot().getStyleClass().add("main-root");
+
+        this.initModality(Modality.APPLICATION_MODAL);
+        this.getIcons().add(new Image("icon.png"));
+        this.setScene(scene);
+    }
+
+    @Override
+    public void handle(Event event) {
+        final Object source = event.getSource();
+
+        // The button to open the handler selection dialog.
+        if(source.equals(view.getButton_addFiles())) {
+            final FileChooser fileChooser = new FileChooser();
+
+            fileChooser.setTitle("Job File Selection");
+            final List<File> selectedFiles = fileChooser.showOpenMultipleDialog(this);
+
+            if(selectedFiles != null) {
+                model.getList_files().addAll(selectedFiles);
+
+                // Add all of the files to the list:
+                for(final File f : selectedFiles) {
+                    view.getListView_selectedFiles().getItems().add(f.getAbsolutePath());
+                }
+            }
+        }
+
+        // The button to remove all files that are currently selected on the scrollpane_selectedFiles.
+        if(source.equals(view.getButton_removeSelectedFiles())) {
+            // If a copy of the observable list is not made, then errors can occur.
+            // These errors are caused by the ObservableList updating as items are being removed.
+            // This causes items that shouldn't be removed to be removed.
+            final ObservableList<String> copy = FXCollections.observableArrayList(view.getListView_selectedFiles().getSelectionModel().getSelectedItems());
+            view.getListView_selectedFiles().getItems().removeAll(copy);
+
+            view.getListView_selectedFiles().getSelectionModel().clearSelection();
+        }
+
+        // The button to remove all files from the list.
+        if(source.equals(view.getButton_clearAllFiles())) {
+            view.getListView_selectedFiles().getItems().clear();
+            view.getListView_selectedFiles().getSelectionModel().clearSelection();
+        }
+
+        // The radio button that says that each of the currently selected files should be archived as a single archive before encoding.
+        if(source.equals(view.getRadioButton_singleArchive_yes())) {
+            // Ensure that both the single and individual archive options aren't
+            // selected at the same time.
+            configHandler.setCombineIntoIndividualArchives(false);
+            view.getRadioButton_individualArchives_no().setSelected(true);
+            configHandler.setCombineAllFilesIntoSingleArchive(true);
+        }
+
+        // The radio button that says that each of the currently selected files should be archived individually before encoding them individually.
+        if(source.equals(view.getRadioButton_singleArchive_no())) {
+            configHandler.setCombineAllFilesIntoSingleArchive(false);
+        }
+
+        // The radio button that says that each handler should be archived individually before encoding each of them individually.
+        if(source.equals(view.getRadioButton_individualArchives_yes())) {
+            // Ensure that both the single and individual archive options aren't
+            // selected at the same time.
+            configHandler.setCombineIntoIndividualArchives(true);
+            view.getRadioButton_singleArchive_no().setSelected(true);
+
+            // Allow the user to edit the archive name/path.
+            configHandler.setCombineAllFilesIntoSingleArchive(false);
+        }
+
+        // The radio button that says that each handler should not be archived individually before encoding each of them individually.
+        if(source.equals(view.getRadioButton_individualArchives_no())) {
+            configHandler.setCombineIntoIndividualArchives(false);
+        }
+
+        // The button to select the folder to output the archive to if the "Yes" radio button is selected.
+        if(source.equals(view.getButton_selectOutputDirectory())) {
+            final JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDragEnabled(false);
+            fileChooser.setMultiSelectionEnabled(false);
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+            fileChooser.setDialogTitle("Directory Slection");
+            fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+
+            fileChooser.setApproveButtonText("Accept");
+            fileChooser.setApproveButtonToolTipText("Accept the selected directory.");
+
+            int returnVal = fileChooser.showOpenDialog(null);
+
+            if(returnVal == JFileChooser.APPROVE_OPTION) {
+                view.getTextField_outputDirectory().setText(fileChooser.getSelectedFile().getPath() + "/");
+            }
+        }
+
+        // The button to close the JobCreationDialog without creating a Job.
+        if(source.equals(view.getButton_accept())) {
+            if(areSettingsCorrect() == false) {
+                final String name = view.getField_jobName().getText();
+                final String description = view.getTextArea_jobDescription().getText();
+                final String outputDirectory = view.getTextField_outputDirectory().getText();
+                final List<File> files = model.getList_files();
+                final boolean isEncodeJob = view.getIsEncodeJob();
+
+                final Job job = new Job(name, description, outputDirectory, files, isEncodeJob);
+                model.setJob(job);
+                this.close();
+            }
+        }
+
+        // The button to close the JobCreationDialog while creating a Job.
+        if(source.equals(view.getButton_cancel())) {
+            model.setJob(null);
+            this.close();
+        }
+
+        updateEstimatedDurationLabel();
+    }
+
+    // todo Javadoc
+    public boolean areSettingsCorrect() {
+        boolean wasErrorFound = false;
+
+        // Reset the error states and tooltips of any components
+        // that can have an error state set.
+        // Set the password fields back to their normal look:
+        view.getField_jobName().getStylesheets().remove("field_error.css");
+        view.getField_jobName().getStylesheets().add("global.css");
+        view.getField_jobName().setTooltip(null);
+
+        view.getTextField_outputDirectory().getStylesheets().remove("field_error.css");
+        view.getTextField_outputDirectory().getStylesheets().add("global.css");
+        view.getTextField_outputDirectory().setTooltip(new Tooltip("The directory in which to place the en/decoded file(s)."));
+
+        // Check to see that the Job has been given a name.
+        if(view.getField_jobName().getText().isEmpty()) {
+            view.getField_jobName().getStylesheets().remove("global.css");
+            view.getField_jobName().getStylesheets().add("field_error.css");
+
+            final String errorText = "Error - You need to enter a name for the Job.";
+            view.getField_jobName().setTooltip(new Tooltip(errorText));
+
+            wasErrorFound = true;
+        }
+
+        // Check to see that the output directory exists:
+        if(view.getTextField_outputDirectory().getText().isEmpty() || Files.exists(Paths.get(view.getTextField_outputDirectory().getText())) == false) {
+            view.getTextField_outputDirectory().getStylesheets().remove("global.css");
+            view.getTextField_outputDirectory().getStylesheets().add("field_error.css");
+
+            final Tooltip currentTooltip = view.getTextField_outputDirectory().getTooltip();
+            final String errorText = "Error - You need to set an output directory for the Job.";
+            view.getField_jobName().setTooltip(new Tooltip(currentTooltip.getText() + "\n\n" + errorText));
+
+            wasErrorFound = true;
+        }
+
+        if(Files.isDirectory(Paths.get(view.getTextField_outputDirectory().getText())) == false) {
+            view.getTextField_outputDirectory().getStylesheets().remove("global.css");
+            view.getTextField_outputDirectory().getStylesheets().add("field_error.css");
+
+            final Tooltip currentTooltip = view.getTextField_outputDirectory().getTooltip();
+            final String errorText = "Error - You need to set a valid output directory for the Job.";
+            view.getField_jobName().setTooltip(new Tooltip(currentTooltip.getText() + "\n\n" + errorText));
+
+            wasErrorFound = true;
+        }
+
+        return wasErrorFound;
+    }
+
+    // todo JavaDoc
+    public void updateEstimatedDurationLabel() {
+        if(model.getJob() != null) {
+            view.getLabel_job_estimatedDurationInMinutes().setText("Estimated Time - " + model.getJob().getEstimatedDurationInMinutes() + " Minutes");
+        } else {
+            view.getLabel_job_estimatedDurationInMinutes().setText("Estimated Time - Unknown");
+        }
+    }
+
+    ////////////////////////////////////////////////////////// Getters
+
+    // todo JavaDoc
+    public JobSetupDialogView getView() {
+        return view;
+    }
+
+    // todo JavaDoc
+    public JobSetupDialogModel getModel() {
+        return model;
+    }
+}
---
+++ src/main/java/view/JobSetupDialogView.java
@@ -0,0 +1,267 @@
+package view;
+
+import controller.JobSetupDialogController;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class JobSetupDialogView extends HBox {
+
+    // todo JavaDoc
+    private ListView<String> listView_selectedFiles = new ListView<>();
+
+    /** The button to open the handler selection dialog. */
+    private final Button button_addFiles = new Button("Add File(s)");
+    /** The button to remove all files that are currently selected on the scrollpane_selectedFiles. */
+    private final Button button_removeSelectedFiles = new Button("Remove File(s)");
+    /** The button to remove all files from the list. */
+    private final Button button_clearAllFiles = new Button("Clear List");
+
+    /** The label to display an estimate of how long the Job will take to run. */
+    private final Label label_job_estimatedDurationInMinutes = new Label("Estimated Time - Unknown");
+
+    /** The button to close the JobCreationDialog while creating a Job. */
+    private final Button button_accept = new Button("Accept");
+    /** The button to close the JobCreationDialog without creating a Job. */
+    private final Button button_cancel = new Button("Cancel");
+
+    // todo JavaDoc
+    private final TextField field_jobName = new TextField();
+
+    /** The comboBox to specify the type of Job to create. */
+    private final ComboBox<String> comboBox_jobType = new ComboBox<>(FXCollections.observableArrayList("Encode", "Decode"));
+
+    // todo JavaDoc
+    private final TextArea textArea_jobDescription = new TextArea();
+
+    /** The toggle group of the "archive all files into a single archive before encoding" yes/no radio buttons. */
+    private final ToggleGroup toggleGroup_singleArchive = new ToggleGroup();
+    /** The radio button that says that each of the currently selected files should be archived as a single archive before encoding. */
+    private final RadioButton radioButton_singleArchive_yes = new RadioButton("Yes");
+    /** The radio button that says that each of the currently selected files should be archived individually before encoding them individually. */
+    private final RadioButton radioButton_singleArchive_no = new RadioButton("No");
+
+    /** The toggle group of the "archive just this handler before encoding" yes/no radio buttons. */
+    private final ToggleGroup toggleGroup_individualArchives = new ToggleGroup();
+    /** The radio button that says that each handler should be archived individually before encoding each of them individually. */
+    private final RadioButton radioButton_individualArchives_yes = new RadioButton("Yes");
+    /** The radio button that says that each handler should not be archived individually before encoding each of them individually. */
+    private final RadioButton radioButton_individualArchives_no = new RadioButton("No");
+
+    /** The text field for the path to the directory in which to place the en/decoded file(s). */
+    private final TextField textField_outputDirectory = new TextField();
+    /** The button to select the folder to output the archive to if the "Yes" radio button is selected. */
+    private final Button button_selectOutputDirectory = new Button("Select Output Folder");
+
+    // todo JavaDoc
+    public JobSetupDialogView(final Stage settingsStage, final JobSetupDialogController controller) {
+        // Setup Job  List:
+        listView_selectedFiles.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        // Setup Toggle Groups:
+        radioButton_singleArchive_yes.setToggleGroup(toggleGroup_singleArchive);
+        radioButton_singleArchive_no.setToggleGroup(toggleGroup_singleArchive);
+
+        radioButton_individualArchives_yes.setToggleGroup(toggleGroup_individualArchives);
+        radioButton_individualArchives_no.setToggleGroup(toggleGroup_individualArchives);
+
+        // Set Text Field/Area Background Text:
+        field_jobName.setPromptText("Enter a name for the Job.");
+        textArea_jobDescription.setPromptText("Enter a description for the Job.\nThis is not required.");
+        textField_outputDirectory.setPromptText("Output Directory");
+
+        // Setup Combo Box:
+        comboBox_jobType.getSelectionModel().select(0);
+
+        // Set Component Tooltips:
+        button_addFiles.setTooltip(new Tooltip("Open the file selection dialog to select a new file."));
+        button_removeSelectedFiles.setTooltip(new Tooltip("Removes all files that are currently selected on the list."));
+        button_clearAllFiles.setTooltip(new Tooltip("Clears the list of all files."));
+        button_accept.setTooltip(new Tooltip("Accepts the Job settings and closes the dialog and creates a Job."));
+        button_cancel.setTooltip(new Tooltip("Rejects the Job settings and closes the dialog without creating a Job."));
+
+        comboBox_jobType.setTooltip(new Tooltip("Defines which type of Job to create."));
+
+        textField_outputDirectory.setTooltip(new Tooltip("The directory in which to place the en/decoded file(s)."));
+        button_selectOutputDirectory.setTooltip(new Tooltip("Open the directory selection dialog to select an output directory."));
+
+        // Set Component EventHandlers:
+        button_addFiles.setOnAction(controller);
+        button_removeSelectedFiles.setOnAction(controller);
+        button_clearAllFiles.setOnAction(controller);
+        button_accept.setOnAction(controller);
+        button_cancel.setOnAction(controller);
+        comboBox_jobType.setOnAction(controller);
+        button_selectOutputDirectory.setOnAction(controller);
+
+        // Setup the Layout:
+        final HBox panel_left_top = new HBox(10);
+        panel_left_top.setAlignment(Pos.CENTER);
+        panel_left_top.getChildren().addAll(button_addFiles, button_removeSelectedFiles, button_clearAllFiles);
+
+        final HBox panel_left_bottom = new HBox(10);
+        panel_left_bottom.setAlignment(Pos.CENTER);
+        panel_left_bottom.getChildren().addAll(button_accept, button_cancel);
+
+        final VBox panel_left = new VBox(4);
+        HBox.setHgrow(panel_left, Priority.ALWAYS);
+        VBox.setVgrow(listView_selectedFiles, Priority.ALWAYS);
+        panel_left.getChildren().addAll(panel_left_top, listView_selectedFiles, panel_left_bottom);
+
+
+
+        final HBox panel_right_top_top = new HBox(10);
+        HBox.setHgrow(field_jobName, Priority.ALWAYS);
+        panel_right_top_top.getChildren().addAll(field_jobName, comboBox_jobType);
+
+        final VBox panel_right_top_bottom= new VBox(4);
+        VBox.setVgrow(textArea_jobDescription, Priority.ALWAYS);
+        VBox.setVgrow(panel_right_top_bottom, Priority.ALWAYS);
+        panel_right_top_bottom.setAlignment(Pos.CENTER);
+        panel_right_top_bottom.getChildren().addAll(textArea_jobDescription, label_job_estimatedDurationInMinutes);
+
+        final VBox panel_right_top = new VBox(4);
+        VBox.setVgrow(panel_right_top,Priority.ALWAYS);
+        panel_right_top.getChildren().addAll(panel_right_top_top, panel_right_top_bottom);
+
+        final HBox pane_panel_singleArchive = new HBox(10);
+        pane_panel_singleArchive.setAlignment(Pos.CENTER);
+        pane_panel_singleArchive.getChildren().addAll(radioButton_singleArchive_yes, radioButton_singleArchive_no);
+
+        final TitledPane pane_singleArchive = new TitledPane();
+        HBox.setHgrow(pane_singleArchive, Priority.ALWAYS);
+        pane_singleArchive.setText("Pack File(s) into Single Archive");
+        pane_singleArchive.setCollapsible(false);
+        pane_singleArchive.heightProperty().addListener(new ChangeListener<Number>() { // Ensures that the scene will rezize when the pane is collapsed.
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                settingsStage.sizeToScene();
+            }
+        });
+        pane_singleArchive.setContent(pane_panel_singleArchive);
+
+        final HBox pane_panel_individualArchives = new HBox(10);
+        pane_panel_individualArchives.setAlignment(Pos.CENTER);
+        pane_panel_individualArchives.getChildren().addAll(radioButton_individualArchives_yes, radioButton_individualArchives_no);
+
+        final TitledPane pane_individualArchives = new TitledPane();
+        HBox.setHgrow(pane_individualArchives, Priority.ALWAYS);
+        pane_individualArchives.setText("Pack File(s) into Single Archive");
+        pane_individualArchives.setCollapsible(false);
+        pane_individualArchives.heightProperty().addListener(new ChangeListener<Number>() { // Ensures that the scene will rezize when the pane is collapsed.
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                settingsStage.sizeToScene();
+            }
+        });
+        pane_individualArchives.setContent(pane_panel_individualArchives);
+
+        final HBox panel_right_bottom_top = new HBox(10);
+        panel_right_bottom_top.getChildren().addAll(pane_singleArchive, pane_individualArchives);
+
+        final HBox panel_right_bottom_bottom = new HBox(10);
+        HBox.setHgrow(textField_outputDirectory, Priority.ALWAYS);
+        panel_right_bottom_bottom.getChildren().addAll(textField_outputDirectory, button_selectOutputDirectory);
+
+        final VBox panel_right_bottom = new VBox(4);
+        panel_right_bottom.getChildren().addAll(panel_right_bottom_top, panel_right_bottom_bottom);
+
+        final VBox panel_right = new VBox(4);
+        HBox.setHgrow(panel_right, Priority.ALWAYS);
+        VBox.setVgrow(textArea_jobDescription, Priority.ALWAYS);
+        panel_right.getChildren().addAll(panel_right_top, panel_right_bottom);
+
+
+
+        this.setSpacing(4);
+        this.getChildren().addAll(panel_left, panel_right);
+    }
+
+    ////////////////////////////////////////////////////////// Getters
+
+    // todo JavaDoc
+    public ListView<String> getListView_selectedFiles() {
+        return listView_selectedFiles;
+    }
+
+    /** @return The button to open the handler selection dialog. */
+    public Button getButton_addFiles() {
+        return button_addFiles;
+    }
+
+    /** @return The button to remove all files that are currently selected on the scrollpane_selectedFiles. */
+    public Button getButton_removeSelectedFiles() {
+        return button_removeSelectedFiles;
+    }
+
+    /** @return The button to remove all files from the list. */
+    public Button getButton_clearAllFiles() {
+        return button_clearAllFiles;
+    }
+
+    /** @return The label to display an estimate of how long the Job will take to run. */
+    public Label getLabel_job_estimatedDurationInMinutes() {
+        return label_job_estimatedDurationInMinutes;
+    }
+
+    /** @return Whether or not the Job is an Encode Job. If not, then it's a Decode Job. */
+    public boolean getIsEncodeJob() {
+        return comboBox_jobType.getSelectionModel().getSelectedItem().equals("Encode");
+    }
+
+    /** @return The button to close the JobCreationDialog while creating a Job. */
+    public Button getButton_accept() {
+        return button_accept;
+    }
+
+    /** @return The button to close the JobCreationDialog without creating a Job. */
+    public Button getButton_cancel() {
+        return button_cancel;
+    }
+
+    // todo JavaDoc
+    public TextField getField_jobName() {
+        return field_jobName;
+    }
+
+    // todo JavaDoc
+    public TextArea getTextArea_jobDescription() {
+        return textArea_jobDescription;
+    }
+
+    /** @return The radio button that says that each of the currently selected files should be archived as a single archive before encoding. */
+    public RadioButton getRadioButton_singleArchive_yes() {
+        return radioButton_singleArchive_yes;
+    }
+
+    /** @return The radio button that says that each of the currently selected files should be archived individually before encoding them individually. */
+    public RadioButton getRadioButton_singleArchive_no() {
+        return radioButton_singleArchive_no;
+    }
+
+    /** @return The radio button that says that each handler should be archived individually before encoding each of them individually. */
+    public RadioButton getRadioButton_individualArchives_yes() {
+        return radioButton_individualArchives_yes;
+    }
+
+    /** @return The radio button that says that each handler should not be archived individually before encoding each of them individually. */
+    public RadioButton getRadioButton_individualArchives_no() {
+        return radioButton_individualArchives_no;
+    }
+
+    /** @return The text field for the path to the directory in which to place the en/decoded file(s). */
+    public TextField getTextField_outputDirectory() {
+        return textField_outputDirectory;
+    }
+
+    /** @return The button to select the folder to output the archive to if the "Yes" radio button is selected. */
+    public Button getButton_selectOutputDirectory() {
+        return button_selectOutputDirectory;
+    }
+}
