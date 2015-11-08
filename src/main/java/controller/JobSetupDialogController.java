796d24f14286e55e7143587614229fe599f37fb7
Valkryst <valkryst@valkryst.com>
2015-11-07 20:09:08 -0400

    Reorganized the radio button EventHandler if-statements.

--- src/main/java/controller/JobSetupDialogController.java
+++ src/main/java/controller/JobSetupDialogController.java
@@ -92,9 +92,9 @@ public class JobSetupDialogController extends Stage implements EventHandler {
         if(source.equals(view.getRadioButton_singleArchive_yes())) {
             // Ensure that both the single and individual archive options aren't
             // selected at the same time.
+            configHandler.setCombineAllFilesIntoSingleArchive(true);
             configHandler.setCombineIntoIndividualArchives(false);
             view.getRadioButton_individualArchives_no().setSelected(true);
-            configHandler.setCombineAllFilesIntoSingleArchive(true);
         }
 
         // The radio button that says that each of the currently selected files should be archived individually before encoding them individually.
@@ -107,10 +107,9 @@ public class JobSetupDialogController extends Stage implements EventHandler {
             // Ensure that both the single and individual archive options aren't
             // selected at the same time.
             configHandler.setCombineIntoIndividualArchives(true);
-            view.getRadioButton_singleArchive_no().setSelected(true);
-
-            // Allow the user to edit the archive name/path.
             configHandler.setCombineAllFilesIntoSingleArchive(false);
+
+            view.getRadioButton_singleArchive_no().setSelected(true);
         }
 
         // The radio button that says that each handler should not be archived individually before encoding each of them individually.
