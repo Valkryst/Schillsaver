95b257725a0140cd018c3a585b4dcdd27cb41bdb
Valkryst <valkryst@valkryst.com>
2015-11-09 17:54:17 -0400

    Removed all variables, comments, and methods related to estimating either encode, or decode, times as the new StatisticsHandler class deals with thsoe functions now.

--- src/main/java/misc/Job.java
+++ src/main/java/misc/Job.java
@@ -4,31 +4,6 @@ import java.io.File;
 import java.util.List;
 
 public class Job {
-    /**
-     * Test #1:
-     *     60MB -> 126MB
-     *     20m
-     *     126MB/20m = ~6.3MB/minute
-     *
-     * Test #2:
-     *     3GB -> 8.7GB = 3000MB - > 8700MB
-     *     23h25m = 1405m
-     *     8700MB/1405m = ~6.2MB/minute
-     *
-     * So a safe estimate is 6MB/minute.
-     * Therefore 6,000,000 bytes/minute are encoded.
-     * Therefore 100,000 bytes/second.
-     */
-    private final static long BYTES_ENCODED_PER_SECOND = 100000;
-
-    /**
-     * Test #1:
-     *     366MB/2m = 183MB/min
-     *     183/60 = 3.05MB/second
-     *     3.05MB = 3,050,000 bytes/second
-     */
-    private final static long BYTES_DECODED_PER_SECOND = 3050000;
-
     /** The unique id of the Job. */
     private int id;
     /** The name of the Job. */
@@ -41,8 +16,6 @@ public class Job {
     private List<File> files;
     /** Whether or not the Job is an Encode Job. If not, then it's a Decode Job. */
     private boolean isEncodeJob;
-    /** A rough estimation of the time, in minutes, it will take for the job to run. */
-    private long estimatedDurationInMinutes;
 
     /** Whether or not to pack all of the currently selected files into a single archive before encoding. */
     private boolean combineAllFilesIntoSingleArchive = false;
@@ -58,34 +31,6 @@ public class Job {
         this.isEncodeJob = isEncodeJob;
         this.combineAllFilesIntoSingleArchive = combineAllFilesIntoSingleArchive;
         this.combineIntoIndividualArchives = combineIntoIndividualArchives;
-    }
-
-    /**
-     * Estimates the time required to encode the file(s) of
-     * the job and assigns the estimate to estimatedDurationInMinutes.
-     */
-    private void estimateEncodingDuration() {
-        long tempEstimation = 0;
-
-        for(final File f : files) {
-            tempEstimation += Math.floor(f.length() / BYTES_ENCODED_PER_SECOND) * 60;
-        }
-
-        estimatedDurationInMinutes = tempEstimation;
-    }
-
-    /**
-     * Estimates the time required to decode the file(s) of
-     * the job and assigns the estimate to estimatedDurationInMinutes.
-     */
-    private void estimateDecodingDuration() {
-        long tempEstimation = 0;
-
-        for(final File f : files) {
-            tempEstimation += Math.floor(f.length() / BYTES_DECODED_PER_SECOND) * 60;
-        }
-
-        estimatedDurationInMinutes = tempEstimation;
     }
 
     ////////////////////////////////////////////////////////// Getters
@@ -129,17 +74,6 @@ public class Job {
     /** @return Whether or not the Job is an Encode Job. If not, then it's a Decode Job. */
     public boolean getIsEncodeJob() {
         return isEncodeJob;
-    }
-
-    /** @return A rough estimation of the time, in minutes, it will take for the job to run. */
-    public long getEstimatedDurationInMinutes() {
-        if(isEncodeJob) {
-            estimateEncodingDuration();
-        } else {
-            estimateDecodingDuration();
-        }
-
-        return estimatedDurationInMinutes;
     }
 
     /** @return Whether or not to combine all of the input files into a single archive when encoding. */
