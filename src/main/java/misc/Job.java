7ecb8fbbb1994eda56d90e1a3149fb2e97cfd978
Valkryst <valkryst@valkryst.com>
2016-07-12 19:59:10 -0300

    Rewrote to use lombok.

--- src/main/java/misc/Job.java
+++ src/main/java/misc/Job.java
@@ -1,26 +1,29 @@
 package misc;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.File;
 import java.util.List;
 
 public class Job {
     /** The unique id of the Job. */
-    private int id;
+    @Getter @Setter private int id;
     /** The name of the Job. */
-    private String name;
+    @Getter private String name;
     /** A rough description of the Job. */
-    private String description;
+    @Getter private String description;
     /** The directory in which to place the output file(s). */
-    private String outputDirectory;
+    @Getter private String outputDirectory;
     /** The file(s) belonging to the Job.*/
-    private List<File> files;
+    @Getter private List<File> files;
     /** Whether or not the Job is an Encode Job. If not, then it's a Decode Job. */
-    private boolean isEncodeJob;
+    @Getter private boolean isEncodeJob;
 
     /** Whether or not to pack all of the currently selected files into a single archive before encoding. */
-    private boolean combineAllFilesIntoSingleArchive = false;
+    @Getter private boolean combineAllFilesIntoSingleArchive = false;
     /** Whether or not to pack every handler into it's own individual archive before encoding each handler individually. */
-    private boolean combineIntoIndividualArchives = false;
+    @Getter private boolean combineIntoIndividualArchives = false;
 
     /**
      * Constructs a new Job with the specified parameters.
@@ -48,8 +51,6 @@ public class Job {
         this.combineIntoIndividualArchives = combineIntoIndividualArchives;
     }
 
-    ////////////////////////////////////////////////////////// Getters
-
     /** @return The full designation of the Job. This includes the unique ID, type, and name. */
     public String getFullDesignation() {
         String s = "";
@@ -59,52 +60,5 @@ public class Job {
         s += " - ";
         s += name;
         return s;
-    }
-
-    /** @return The unique id of the Job. */
-    public int getId() {
-        return id;
-    }
-
-    /** @return The name of the Job. */
-    public String getName() {
-        return name;
-    }
-
-    /** @return A rough description of the Job. */
-    public String getDescription() {
-        return description;
-    }
-
-    /** @return The directory in which to place the output files.  */
-    public String getOutputDirectory() {
-        return outputDirectory;
-    }
-
-    /** @return The file(s) belonging to the Job.*/
-    public List<File> getFiles() {
-        return files;
-    }
-
-    /** @return Whether or not the Job is an Encode Job. If not, then it's a Decode Job. */
-    public boolean getIsEncodeJob() {
-        return isEncodeJob;
-    }
-
-    /** @return Whether or not to combine all of the input files into a single archive when encoding. */
-    public boolean getCombineAllFilesIntoSingleArchive() {
-        return combineAllFilesIntoSingleArchive;
-    }
-
-    /** @return Whether or not to pack every handler into it's own individual archive before encoding each handler individually. */
-    public boolean getCombineIntoIndividualArchives() {
-        return combineIntoIndividualArchives;
-    }
-
-    ////////////////////////////////////////////////////////// Setters
-
-    // todo JavaDoc
-    public void setId(final int id) {
-        this.id = id;
     }
 }
