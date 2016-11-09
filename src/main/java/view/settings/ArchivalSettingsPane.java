cb1ac470f263c340d41744650902d67b4f1e12cd
Valkryst <valkryst@valkryst.com>
2016-11-09 16:52:29 -0400

    Added new text field for the output extension.
    Updated prompt text.

--- src/main/java/view/settings/ArchivalSettingsPane.java
+++ src/main/java/view/settings/ArchivalSettingsPane.java
@@ -2,8 +2,6 @@ package view.settings;
 
 import controller.settings.ArchivalSettingsController;
 import handler.ConfigHandler;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
@@ -26,10 +24,14 @@ public class ArchivalSettingsPane extends TitledPane {
     /** The text field for the base commands to use when compressing a file before encoding. */
     @Getter private final TextField field_compressionCommands = new TextField();
 
+    /** The text field for the extension to use when outputting an archive. */
+    @Getter private final TextField field_archiveOutputExtension = new TextField();
+
     public ArchivalSettingsPane(final Stage settingsStage, final ArchivalSettingsController controller, final ConfigHandler configHandler) {
         // Set Field Prompt Text:
-        field_compressionProgramPath.setPromptText("compressionProgramPath");
-        field_compressionCommands.setPromptText("compressionCommands");
+        field_compressionProgramPath.setPromptText("Archive Program Path");
+        field_compressionCommands.setPromptText("Archive Commandline Parameters");
+        field_archiveOutputExtension.setPromptText("Archive Output Extension");
 
         // Set Default Values:
         field_compressionProgramPath.setText(configHandler.getCompressionProgramPath());
@@ -40,6 +42,8 @@ public class ArchivalSettingsPane extends TitledPane {
         button_selectFile_compressionProgramPath.setTooltip(new Tooltip("Opens the file selection dialog to locate a compression program executable."));
 
         field_compressionCommands.setTooltip(new Tooltip("The base commands to use when compressing a file before encoding."));
+
+        field_archiveOutputExtension.setTooltip(new Tooltip("The extension to use when outputting an archive. If this is \"7z\" then the output is \"file.7z\"."));
 
         // Set Component Listeners:
         button_selectFile_compressionProgramPath.setOnAction(controller);
@@ -52,17 +56,12 @@ public class ArchivalSettingsPane extends TitledPane {
 
         final VBox panel = new VBox(4);
         HBox.setHgrow(field_compressionCommands, Priority.ALWAYS);
-        panel.getChildren().addAll(panel_top, field_compressionCommands);
+        panel.getChildren().addAll(panel_top, field_compressionCommands, field_archiveOutputExtension);
 
 
         this.setText(TAB_NAME);
         this.setCollapsible(false);
-        this.heightProperty().addListener(new ChangeListener<Number>() { // Ensures that the scene will rezize when the pane is collapsed.
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                settingsStage.sizeToScene();
-            }
-        });
+        this.heightProperty().addListener((observable, oldValue, newValue) -> settingsStage.sizeToScene());
         this.setContent(panel);
     }
 }
