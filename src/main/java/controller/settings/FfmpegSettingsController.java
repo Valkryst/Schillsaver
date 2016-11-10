4389a3377428b0f9a07bbdeab9e99387e5cb5d42
Valkryst <valkryst@valkryst.com>
2016-11-10 00:56:40 -0400

    Reworked a few sections of code to avoid code duplication and to rely on RegEX matching rather than a try-catch for verifying if a TextField contains a positive integer.

--- src/main/java/controller/settings/FfmpegSettingsController.java
+++ src/main/java/controller/settings/FfmpegSettingsController.java
@@ -4,6 +4,7 @@ import handler.ConfigHandler;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -13,6 +14,7 @@ import view.settings.FfmpegSettingsPane;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 public class FfmpegSettingsController implements EventHandler {
     // todo JavaDoc
@@ -58,46 +60,36 @@ public class FfmpegSettingsController implements EventHandler {
         // Reset the error states and tooltips of any components
         // that can have an error state set.
         // Set the password fields back to their normal look:
-        pane.getField_ffmpegPath().getStylesheets().remove("field_error.css");
-        pane.getField_ffmpegPath().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_ffmpegPath());
         pane.getField_ffmpegPath().setTooltip(new Tooltip("The absolute path to ffmpeg/ffmpeg.exe."));
 
-        pane.getField_encodeFormat().getStylesheets().remove("field_error.css");
-        pane.getField_encodeFormat().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_encodeFormat());
         pane.getField_encodeFormat().setTooltip(new Tooltip("The format to encode to."));
 
-        pane.getField_decodeFormat().getStylesheets().remove("field_error.css");
-        pane.getField_decodeFormat().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_decodeFormat());
         pane.getField_decodeFormat().setTooltip(new Tooltip("The format to decode to.\n\nThis should be the handler that your archival program archives to.\nWith 7zip, this should be set to 7z."));
 
-        pane.getField_encodedVideoWidth().getStylesheets().remove("field_error.css");
-        pane.getField_encodedVideoWidth().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_encodedVideoWidth());
         pane.getField_encodedVideoWidth().setTooltip(new Tooltip("The width, in pixels, of the encoded video."));
 
-        pane.getField_encodedVideoHeight().getStylesheets().remove("field_error.css");
-        pane.getField_encodedVideoHeight().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_encodedVideoHeight());
         pane.getField_encodedVideoHeight().setTooltip(new Tooltip("The height, in pixels, of the encoded video."));
 
-        pane.getField_encodedFramerate().getStylesheets().remove("field_error.css");
-        pane.getField_encodedFramerate().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_encodedFramerate());
         pane.getField_encodedFramerate().setTooltip(new Tooltip("The framerate of the encoded video. Ex ~ 30, 60, etc..."));
 
-        pane.getField_macroBlockDimensions().getStylesheets().remove("field_error.css");
-        pane.getField_macroBlockDimensions().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_macroBlockDimensions());
         pane.getField_macroBlockDimensions().setTooltip(new Tooltip("The width/height of each encoded macroblock."));
 
-        pane.getField_encodingLibrary().getStylesheets().remove("field_error.css");
-        pane.getField_encodingLibrary().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_encodingLibrary());
         pane.getField_encodingLibrary().setTooltip(new Tooltip("The library to encode the video with."));
 
-        pane.getField_fullyCustomFfmpegEncodingOptions().getStylesheets().remove("field_error.css");
-        pane.getField_fullyCustomFfmpegEncodingOptions().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_fullyCustomFfmpegEncodingOptions());
         pane.getField_fullyCustomFfmpegEncodingOptions().setTooltip(new Tooltip("The commands to use when encoding a file with ffmpeg.\n\n" +
                                                                                "If the fully-custom options are enabled, then all other ffmpeg options are ignored\n" +
                                                                                "and this string will be used as the only argument to ffmpeg when encoding."));
 
-        pane.getField_fullyCustomFfmpegDecodingptions().getStylesheets().remove("field_error.css");
-        pane.getField_fullyCustomFfmpegDecodingptions().getStylesheets().add("global.css");
+        removeErrorCSSStyle(pane.getField_fullyCustomFfmpegDecodingptions());
         pane.getField_fullyCustomFfmpegDecodingptions().setTooltip(new Tooltip("The commands to use when encoding a file with ffmpeg.\n\n" +
                                                                               "If the fully-custom options are enabled, then all other ffmpeg options are ignored\n" +
                                                                               "and this string will be used as the only argument to ffmpeg when encoding."));
@@ -105,8 +97,7 @@ public class FfmpegSettingsController implements EventHandler {
 
         // Check to see if the path to ffmpeg actually leads to something.
         if(pane.getField_ffmpegPath().getText().isEmpty() || Files.exists(Paths.get(pane.getField_ffmpegPath().getText())) == false) {
-            pane.getField_ffmpegPath().getStylesheets().remove("global.css");
-            pane.getField_ffmpegPath().getStylesheets().add("field_error.css");
+            applyErrorCSSStyle(pane.getField_ffmpegPath());
 
             final Tooltip currentTooltip = pane.getField_ffmpegPath().getTooltip();
             final String errorText = "Error - You must set the location of FFMPEG.";
@@ -119,123 +110,70 @@ public class FfmpegSettingsController implements EventHandler {
         // Else check all of the other settings.
         if(pane.getRadioButton_useFullyCustomEncodingOptions_yes().isSelected()) {
             if(! pane.getField_fullyCustomFfmpegEncodingOptions().getText().isEmpty()) {
-                pane.getField_fullyCustomFfmpegEncodingOptions().getStylesheets().remove("global.css");
-                pane.getField_fullyCustomFfmpegEncodingOptions().getStylesheets().add("field_error.css");
+                final String errorMessage = "Error - Because you have enabled the fully-custom FFMPEG settings you must " +
+                                            "enter your own encoding commands\nRead the readme for more information.";
 
-                final Tooltip currentTooltip = pane.getField_fullyCustomFfmpegEncodingOptions().getTooltip();
-                final String errorText = "Error - Because you have enabled the fully-custom FFMPEG settings you must enter your own encoding commands\nRead the readme for more information.";
-                pane.getField_fullyCustomFfmpegEncodingOptions().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
-
+                applyErrorCSSStyle(pane.getField_fullyCustomFfmpegEncodingOptions());
+                appendErrorMessageToTooltip(pane.getField_fullyCustomFfmpegEncodingOptions(), errorMessage);
                 wasErrorFound = true;
             }
 
             if(! pane.getField_fullyCustomFfmpegDecodingptions().getText().isEmpty()) {
-                pane.getField_fullyCustomFfmpegDecodingptions().getStylesheets().remove("global.css");
-                pane.getField_fullyCustomFfmpegDecodingptions().getStylesheets().add("field_error.css");
+                final String errorMessage = "Error - Because you have enabled the fully-custom FFMPEG settings you must " +
+                                            "enter your own decoding commands\nRead the readme for more information.";
 
-                final Tooltip currentTooltip = pane.getField_fullyCustomFfmpegDecodingptions().getTooltip();
-                final String errorText = "Error - Because you have enabled the fully-custom FFMPEG settings you must enter your own decoding commands\nRead the readme for more information.";
-                pane.getField_fullyCustomFfmpegDecodingptions().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
-
+                applyErrorCSSStyle(pane.getField_fullyCustomFfmpegDecodingptions());
+                appendErrorMessageToTooltip(pane.getField_fullyCustomFfmpegDecodingptions(), errorMessage);
                 wasErrorFound = true;
             }
         } else {
             // Check to see if encode, decode, encoding library fields have been set.
             if(pane.getField_encodeFormat().getText().isEmpty()) {
-                pane.getField_encodeFormat().getStylesheets().remove("global.css");
-                pane.getField_encodeFormat().getStylesheets().add("field_error.css");
-
-                final Tooltip currentTooltip = pane.getField_encodeFormat().getTooltip();
-                final String errorText = "Error - You must enter an encode format.";
-                pane.getField_encodeFormat().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
+                applyErrorCSSStyle(pane.getField_encodeFormat());
+                appendErrorMessageToTooltip(pane.getField_encodeFormat(), "Error - You must enter an encode format.");
             }
 
             if(pane.getField_decodeFormat().getText().isEmpty()) {
-                pane.getField_decodeFormat().getStylesheets().remove("global.css");
-                pane.getField_decodeFormat().getStylesheets().add("field_error.css");
-
-                final Tooltip currentTooltip = pane.getField_decodeFormat().getTooltip();
-                final String errorText = "Error - You must enter an decode format.";
-                pane.getField_decodeFormat().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
+                applyErrorCSSStyle(pane.getField_decodeFormat());
+                appendErrorMessageToTooltip(pane.getField_encodeFormat(), "Error - You must enter an decode format.");
             }
 
             if(pane.getField_encodingLibrary().getText().isEmpty()) {
-                pane.getField_encodingLibrary().getStylesheets().remove("global.css");
-                pane.getField_encodingLibrary().getStylesheets().add("field_error.css");
-
-                final Tooltip currentTooltip = pane.getField_encodingLibrary().getTooltip();
-                final String errorText = "Error - You must enter an encoding library.";
-                pane.getField_encodingLibrary().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
+                applyErrorCSSStyle(pane.getField_encodingLibrary());
+                appendErrorMessageToTooltip(pane.getField_encodeFormat(), "Error - You must enter an encoding library.");
             }
 
-            // Check to see that all number-fields are actually numbers.
-            // Check to see that all number-fields contain acceptable numbers.
-            // Set their error states to true if they don't.
-            try {
-                int temp = Integer.valueOf(pane.getField_encodedVideoWidth().getText());
+            // Check to see that all number-fields are positive numbers and set their error
+            // tooltips and CSS styles if they don't.
+            final String errorMessage = "Error - There is no integer entered here. Please enter an integer of 1 or greater.";
 
-                if(temp < 1) {
-                    throw new NumberFormatException(""); // Throw an empty exception to trigger error handling.
-                }
-            } catch(final NumberFormatException e) {
-                pane.getField_encodedVideoWidth().getStylesheets().remove("global.css");
-                pane.getField_encodedVideoWidth().getStylesheets().add("field_error.css");
 
-                final Tooltip currentTooltip = pane.getField_encodedVideoWidth().getTooltip();
-                final String errorText = "Error - There is no integer entered here. Please enter an integer of 1 or greater.";
-                pane.getField_encodedVideoWidth().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
+            if (containsPositiveInteger(pane.getField_encodedVideoWidth()) == false) {
+                applyErrorCSSStyle(pane.getField_encodedVideoWidth());
+                appendErrorMessageToTooltip(pane.getField_encodedVideoWidth(), errorMessage);
 
                 wasErrorFound = true;
             }
 
-            try {
-                int temp = Integer.valueOf(pane.getField_encodedVideoHeight().getText());
 
-                if(temp < 1) {
-                    throw new NumberFormatException(""); // Throw an empty exception to trigger error handling.
-                }
-            } catch(final NumberFormatException e) {
-                pane.getField_encodedVideoHeight().getStylesheets().remove("global.css");
-                pane.getField_encodedVideoHeight().getStylesheets().add("field_error.css");
-
-                final Tooltip currentTooltip = pane.getField_encodedVideoHeight().getTooltip();
-                final String errorText = "Error - There is no integer entered here. Please enter an integer of 1 or greater.";
-                pane.getField_encodedVideoHeight().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
+            if (containsPositiveInteger(pane.getField_encodedVideoHeight()) == false) {
+                applyErrorCSSStyle(pane.getField_encodedVideoHeight());
+                appendErrorMessageToTooltip(pane.getField_encodedVideoHeight(), errorMessage);
 
                 wasErrorFound = true;
             }
 
-            try {
-                int temp = Integer.valueOf(pane.getField_encodedFramerate().getText());
-
-                if(temp < 1) {
-                    throw new NumberFormatException(""); // Throw an empty exception to trigger error handling.
-                }
-            } catch(final NumberFormatException e) {
-                pane.getField_encodedFramerate().getStylesheets().remove("global.css");
-                pane.getField_encodedFramerate().getStylesheets().add("field_error.css");
-
-                final Tooltip currentTooltip = pane.getField_encodedFramerate().getTooltip();
-                final String errorText = "Error - There is no integer entered here. Please enter an integer of 1 or greater.";
-                pane.getField_encodedFramerate().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
+            if (containsPositiveInteger(pane.getField_encodedFramerate()) == false) {
+                applyErrorCSSStyle(pane.getField_encodedFramerate());
+                appendErrorMessageToTooltip(pane.getField_encodedFramerate(), errorMessage);
 
                 wasErrorFound = true;
             }
 
-            try {
-                int temp = Integer.valueOf(pane.getField_macroBlockDimensions().getText());
 
-                if(temp < 1) {
-                    throw new NumberFormatException(""); // Throw an empty exception to trigger error handling.
-                }
-            } catch(final NumberFormatException e) {
-                pane.getField_macroBlockDimensions().getStylesheets().remove("global.css");
-                pane.getField_macroBlockDimensions().getStylesheets().add("field_error.css");
-
-                final Tooltip currentTooltip = pane.getField_macroBlockDimensions().getTooltip();
-                final String errorText = "Error - There is no integer entered here. Please enter an integer of 1 or greater.";
-                pane.getField_macroBlockDimensions().setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorText));
-
+            if (containsPositiveInteger(pane.getField_macroBlockDimensions()) == false) {
+                applyErrorCSSStyle(pane.getField_macroBlockDimensions());
+                appendErrorMessageToTooltip(pane.getField_macroBlockDimensions(), errorMessage);
                 wasErrorFound = true;
             }
         }
@@ -318,5 +256,59 @@ public class FfmpegSettingsController implements EventHandler {
                 alert.showAndWait();
             }
         }
+    }
+
+    /**
+     * Determines whether or not the specified TextField contains a positive
+     * integer.
+     *
+     * @param textField
+     *         The TextField whose contents are to be checked.
+     *
+     * @return
+     *         Whether or not the specified TextField contains a positive
+     *         integer.
+     */
+    private boolean containsPositiveInteger(final TextField textField) {
+        return Pattern.matches("^[1-9]+[0-9]*$", textField.getText());
+    }
+
+    /**
+     * Removes the normal CSS styling and applies the error CSS styling to the
+     * specified TextField.
+     *
+     * @param textField
+     *         The TextField to apply the error CSS styling to.
+     */
+    private void applyErrorCSSStyle(final TextField textField) {
+        textField.getStylesheets().remove("global.css");
+        textField.getStylesheets().add("field_error.css");
+    }
+
+    /**
+     * Removes the error CSS styling and applies the normal CSS styling to the
+     * specified TextField.
+     *
+     * @param textField
+     *         The TextField to apply the normal CSS styling to.
+     */
+    private void removeErrorCSSStyle(final TextField textField) {
+        textField.getStylesheets().remove("field_error.css");
+        textField.getStylesheets().add("global.css");
+    }
+
+    /**
+     * Appends the specified error message to the Tooltip of the specified
+     * TextField.
+     *
+     * @param textField
+     *         The TextField whose Tooltip is to be appended to.
+     *
+     * @param errorMessage
+     *         The error message to append to the Tooltip.
+     */
+    private void appendErrorMessageToTooltip(final TextField textField, final String errorMessage) {
+        final Tooltip currentTooltip = textField.getTooltip();
+        textField.setTooltip(new Tooltip(currentTooltip.getText() + "\n" + errorMessage));
     }
 }
