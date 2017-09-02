b8775bd4f082a2ff7686fa958c454d9d1cfa4aae
Valkryst <valkryst@valkryst.com>
2017-09-02 18:09:02 -0300

    Removes redundant custom sorting algorithm.

--- src/handler/JobHandler.java
+++ src/handler/JobHandler.java
@@ -4,7 +4,7 @@ import controller.MainScreenController;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class JobHandler extends Task {
@@ -23,9 +23,7 @@ public class JobHandler extends Task {
         this.controller = controller;
         this.preparedTasks = preparedTasks;
 
-
-        // Sort the Jobs from smalles to largest:
-        greedySort(this.preparedTasks);
+        preparedTasks.sort(Comparator.comparingLong(FFMPEGHandler::getTotalFilesize));
     }
 
     @Override
@@ -61,70 +59,5 @@ public class JobHandler extends Task {
         });
 
         return null;
-    }
-
-    /**
-     * To ensure that the smallest Jobs are run first, the greedy
-     * algorithm sorts the array by smallest total filesize using
-     * mergesort.
-     *
-     * @param arr The Job(s) to sort.
-     * @return A sorted list of Job(s) from smallest to largest filesize.
-     */
-    private List<FFMPEGHandler> greedySort(final List<FFMPEGHandler> arr) {
-        // If the array has zero, or one, element, then it is already sorted.
-        if(arr.size() == 0 || arr.size() == 1) {
-            return arr;
-        }
-
-        // Split the array into two halves.
-        int half = arr.size()/2;
-        List<FFMPEGHandler> arrA = new ArrayList<>();
-        List<FFMPEGHandler> arrB = new ArrayList<>();
-
-        // Fill both halves with the values from arr.
-        for(int i=0;i<half;i++) {
-            arrA.add(arr.get(i));
-        }
-
-        for(int i=half;i<arr.size();i++) {
-            arrB.add(i - half, arr.get(i));
-        }
-
-        // Recursively call the sort() method on both halves.
-        arrA = greedySort(arrA);
-        arrB = greedySort(arrB);
-
-        // Combine the two sorted arrays while sorting.
-        List<FFMPEGHandler> arrC = new ArrayList<>();
-
-        int arrAIndex = 0, arrBIndex = 0, arrCIndex = 0;
-
-        while(arrAIndex < arrA.size() && arrBIndex < arrB.size()) {
-            if(arrA.get(arrAIndex).getTotalFilesize() < arrB.get(arrBIndex).getTotalFilesize()) {
-                arrC.add(arrCIndex, arrA.get(arrAIndex));
-                arrAIndex++;
-            } else {
-                arrC.add(arrCIndex, arrB.get(arrBIndex));
-                arrBIndex++;
-            }
-            arrCIndex++;
-        }
-
-        // Because the above loop doesn't work for all elements we must
-        // copy whatever elements remain into arrC.
-        while(arrAIndex < arrA.size()) {
-            arrC.add(arrCIndex, arrA.get(arrAIndex));
-            arrCIndex++;
-            arrAIndex++;
-        }
-
-        while(arrBIndex < arrB.size()) {
-            arrC.add(arrCIndex, arrB.get(arrBIndex));
-            arrCIndex++;
-            arrBIndex++;
-        }
-
-        return arrC;
     }
 }
