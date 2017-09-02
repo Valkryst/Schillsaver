f5832b95101a7bdeb0d7983bd33f14be9f567c17
Valkryst <valkryst@valkryst.com>
2017-09-02 18:00:04 -0300

    Removes redundant code.

--- src/model/JobSetupDialogModel.java
+++ src/model/JobSetupDialogModel.java
@@ -1,5 +1,8 @@
 package model;
 
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 import misc.Job;
 
 import java.io.File;
@@ -8,22 +11,8 @@ import java.util.List;
 
 public class JobSetupDialogModel {
     /** The Job being created by the JobSetupDialog. */
-    private Job job = null;
+    @Getter @Setter @NonNull private Job job = null;
+
     /** The list of all list_files to be added to the Job. */
-    private List<File> list_files = new ArrayList<>();
-
-    /** @return The Job being created by the JobSetupDialog. */
-    public Job getJob() {
-        return job;
-    }
-
-    /** @return A list of all list_files to be added to the Job. */
-    public List<File> getList_files() {
-        return list_files;
-    }
-
-    /** @param job The new Job being created by the JobSetupDialog. */
-    public void setJob(final Job job) {
-        this.job = job;
-    }
+    @Getter private List<File> list_files = new ArrayList<>();
 }
