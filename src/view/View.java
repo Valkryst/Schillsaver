b8164cc153cbbbeab876a13e69458775d0225437
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-05 13:00:27 -0400

    Initial commit of the View class.

---
+++ src/view/View.java
@@ -0,0 +1,33 @@
+package view;
+
+import javafx.scene.control.Control;
+import javafx.scene.control.Tooltip;
+
+import java.util.Objects;
+
+public class View {
+    /**
+     * Sets the tooltip of a JavaFX control.
+     *
+     * @param control
+     *          The control.
+     *
+     * @param message
+     *          The message.
+     *
+     * @throws java.lang.NullPointerException
+     *           If the message is null.
+     *
+     * @throws java.lang.IllegalArgumentException
+     *           If the message is empty.
+     */
+    private void setTooltip(final Control control, final String message) {
+        Objects.requireNonNull(message);
+
+        if (message.isEmpty()) {
+            throw new IllegalArgumentException("The message cannot be empty.");
+        }
+
+        control.setTooltip(new Tooltip(message));
+    }
+}
