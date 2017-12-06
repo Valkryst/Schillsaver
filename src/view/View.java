ca6164a04d5f214ba62232a17f20b9cf658ab72f
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-06 15:40:52 -0400

    Adds constructor to View.

--- src/view/View.java
+++ src/view/View.java
@@ -11,6 +11,11 @@ public class View {
     /** The pane of the view. */
     @Getter protected Pane pane;
 
+    /** Constructs a new View. */
+    View() {
+        pane.setMinSize(512, 512);
+    }
+
     /**
      * Sets the tooltip of a JavaFX control.
      *
