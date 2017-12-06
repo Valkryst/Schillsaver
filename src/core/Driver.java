fb4dd4c2c9d14c557f55fa72e9490aec7609699f
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-06 10:51:32 -0400

    Adds the swapToNewScene and swapToPreviousScene functions.

--- src/core/Driver.java
+++ src/core/Driver.java
@@ -10,6 +10,14 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Driver extends Application {
+    /** The primary stage. */
+    private Stage primaryStage;
+
+    /** The previous scene. */
+    private Scene previousScene;
+    /** The current scene. */
+    private Scene currentScene;
+
     public static void main(final String[] args) {
         launch();
     }
@@ -18,7 +26,9 @@ public class Driver extends Application {
     public void init() {}
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(final Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+
         // Load Config File:
         final Settings settings = new Settings();
         settings.loadFromFile();
@@ -32,11 +42,39 @@ public class Driver extends Application {
         // Add the first scene to the primary stage:
         final Scene scene = new Scene(new MainController(primaryStage).getView());
 
-        scene.getStylesheets().add("global.css");
-        scene.getRoot().getStyleClass().add("main-root");
-
         primaryStage.setTitle("Schillsaver - Powered by /g/entoomen\u00a9\u00ae");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    /**
+     * Swaps the current scene with a new scene, so that the new scene
+     * is displayed.
+     *
+     * @param newScene
+     *          The new scene.
+     */
+    public void swapToNewScene(final Scene newScene) {
+        previousScene = currentScene;
+        currentScene = newScene;
+
+        if (currentScene.getStylesheets().size() == 0) {
+            newScene.getStylesheets().add("global.css");
+            newScene.getRoot().getStyleClass().add("main-root");
+        }
+
+        primaryStage.setScene(currentScene);
+    }
+
+    /**
+     * Swaps the current and previous scenes, so that the previous scene
+     * is displayed.
+     */
+    public void swapToPreviousScene() {
+        final Scene temp = previousScene;
+        previousScene = currentScene;
+        currentScene = temp;
+
+        primaryStage.setScene(currentScene);
     }
 }
