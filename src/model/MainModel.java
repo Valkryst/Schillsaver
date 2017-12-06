a80dac27917992a63b0a90008c98870e570eb472
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-06 12:27:00 -0400

    Updates MainModel to extend Model.

--- src/model/MainModel.java
+++ src/model/MainModel.java
@@ -7,7 +7,7 @@ import misc.Job;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainModel {
+public class MainModel extends Model {
     /** The jobs. */
     @Getter @Setter private Map<String, Job> jobs = new HashMap<>();
 }
