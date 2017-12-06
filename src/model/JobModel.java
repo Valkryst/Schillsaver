94f4119aafe98e7fad27b0e7d59250fa5f078e4b
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-06 15:06:39 -0400

    Adds files list to model.

--- src/model/JobModel.java
+++ src/model/JobModel.java
@@ -1,4 +1,12 @@
 package model;
 
+import lombok.Getter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JobModel extends Model {
+    /** The job's files. */
+    @Getter private final List<File> files = new ArrayList<>();
 }
