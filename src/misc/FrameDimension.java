20bc708191c110dc2d37b8bc5f0bbbb583041963
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-07 13:32:14 -0400

    Initial commit of the FrameDimension enum.

---
+++ src/misc/FrameDimension.java
@@ -0,0 +1,31 @@
+package misc;
+
+import lombok.Getter;
+
+import java.awt.Dimension;
+
+public enum FrameDimension {
+    P240(426, 240),
+    P360(640, 360),
+    P480(854, 480),
+    P720(1280, 720),
+    P1080(1920, 1080),
+    P1440(2560, 1440),
+    P2160(3840, 2160);
+
+    /** The frame dimensions. */
+    @Getter private final Dimension dimensions;
+
+    /**
+     * Construct a new FrameDimension enum.
+     *
+     * @param width
+     *          The frame width.
+     *
+     * @param height
+     *          The frame height.
+     */
+    FrameDimension(final int width, final int height) {
+        dimensions = new Dimension(width, height);
+    }
+}
