addc0898d544a54c519aad436ab605c7ab919de7
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-07 10:35:50 -0400

    Adds the load/saveJobs function.

--- src/model/MainModel.java
+++ src/model/MainModel.java
@@ -3,11 +3,52 @@ package model;
 import lombok.Getter;
 import lombok.Setter;
 import misc.Job;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainModel extends Model {
     /** The jobs. */
     @Getter @Setter private Map<String, Job> jobs = new HashMap<>();
+
+    /** Deserializes the jobs map, if the file exists. */
+    public void loadJobs() {
+        final String filePath = System.getProperty("user.dir") + "/jobs.ser";
+
+        try (
+            final FileInputStream fis = new FileInputStream(filePath);
+            final ObjectInputStream ois = new ObjectInputStream(fis);
+        ) {
+            final Object object = ois.readObject();
+            jobs = (Map<String, Job>) object;
+        } catch (IOException | ClassNotFoundException e) {
+            final Logger logger = LogManager.getLogger();
+            logger.error(e);
+
+            // Delete the file:
+            final File file = new File(filePath);
+
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+
+    /** Serializes the jobs map to a file. */
+    public void saveJobs() {
+        final String filePath = System.getProperty("user.dir") + "/jobs.ser";
+
+        try (
+            final FileOutputStream fos = new FileOutputStream(filePath, false);
+            final ObjectOutputStream oos = new ObjectOutputStream(fos);
+        ) {
+            oos.writeObject(jobs);
+        } catch (IOException e) {
+            final Logger logger = LogManager.getLogger();
+            logger.error(e);
+        }
+    }
 }
