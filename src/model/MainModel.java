8c80cc7102269c5dfd79fb51a287dbbc9cd1fbad
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-08 12:59:02 -0400

    Adds functions to prepare en/decode jobs.

--- src/model/MainModel.java
+++ src/model/MainModel.java
@@ -1,10 +1,16 @@
 package model;
 
+import configuration.Settings;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
 import lombok.Getter;
 import lombok.Setter;
 import misc.Job;
+import misc.VideoEncoder;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import view.MainView;
 
 import java.io.*;
 import java.util.*;
@@ -62,13 +68,70 @@ public class MainModel extends Model {
         }
     }
 
+    public List<Thread> prepareEncodingJobs(final Settings settings, final MainView view) {
+        final List<Thread> encodingJobs = new ArrayList<>();
+
+        for (final Job job : getEncodingJobs()) {
+            final Tab tab = view.addOutputTab(job.getName());
+
+            final Thread thread = new Thread(() -> {
+                try {
+                    tab.setClosable(false);
+
+                    final VideoEncoder videoEncoder = new VideoEncoder(settings, (TextArea) tab.getContent());
+
+                    final File inputFile = job.zipFiles();
+                    final File outputFile = new File(job.getOutputDirectory() + FilenameUtils.removeExtension(inputFile.getName()) + ".mp4");
+
+                    videoEncoder.encode(inputFile, outputFile);
+
+                    inputFile.delete();
+                    tab.setClosable(true);
+                } catch (final IOException | InterruptedException e) {
+                    // todo Couldn't zip the files, abort, log, tell user.
+                    // todo Could also be an exception when opening muxer in the vid encoder class.
+                    final Logger logger = LogManager.getLogger();
+                    logger.error(e);
+
+                    e.printStackTrace();
+                }
+            });
+
+            encodingJobs.add(thread);
+        }
+
+        return encodingJobs;
+    }
+
+    public List<Thread> prepareDecodingJobs(final Settings settings, final MainView view) {
+        final List<Thread> decodingJobs = new ArrayList<>();
+
+        for (final Job job : getDecodingJobs()) {
+            final Tab tab = view.addOutputTab(job.getName());
+
+            final Thread thread = new Thread(() -> {
+                tab.setClosable(false);
+
+                // todo WRITE DECODE CODE
+                System.err.println("DECODE STUFF NOT WRITTEN IN MAINMODEL");
+                System.exit(1);
+
+                tab.setClosable(true);
+            });
+
+            decodingJobs.add(thread);
+        }
+
+        return decodingJobs;
+    }
+
     /**
      * Retrieves an unmodifiable list of encoding jobs.
      *
      * @return
      *         The list of encoding jobs.
      */
-    public List<Job> getEncodingJobs() {
+    private List<Job> getEncodingJobs() {
         final List<Job> encodingJobs = new ArrayList<>();
 
         for (final Job job : jobs.values()) {
@@ -86,7 +149,7 @@ public class MainModel extends Model {
      * @return
      *         The list of decoding jobs.
      */
-    public List<Job> getDecodingJobs() {
+    private List<Job> getDecodingJobs() {
         final List<Job> decodingJobs = new ArrayList<>();
 
         for (final Job job : jobs.values()) {
