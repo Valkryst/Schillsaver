8de221343e5f9b327a77aec38004b39a22a028f4
Valkryst <valkryst@valkryst.com>
2017-12-09 11:04:48 -0400

    Adds check for compatible block size to constructor.

--- src/misc/VideoEncoder.java
+++ src/misc/VideoEncoder.java
@@ -50,6 +50,8 @@ public class VideoEncoder {
         final FrameDimension frameDimensions = settings.getFrameDimensions();
         final Dimension blockDimensions = settings.getBlockDimensions();
 
+        frameDimensions.isCompatibleBlockSize(blockDimensions);
+
         rows = frameDimensions.getHeight() / blockDimensions.height;
         columns = frameDimensions.getWidth() / blockDimensions.width;
         bitsPerFrame = (columns * rows) / 8;
