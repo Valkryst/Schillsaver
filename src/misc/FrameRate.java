a1848750c5606055c185217f5daf3f811e20f3d0
Valkryst <valkryst@valkryst.com>
2017-12-09 12:07:45 -0400

    Adds the getFrameRate function.

--- src/misc/FrameRate.java
+++ src/misc/FrameRate.java
@@ -19,4 +19,26 @@ public enum FrameRate {
     FrameRate(final int frameRate) {
         this.frameRate = Rational.make(1, frameRate);
     }
+
+    /**
+     * Returns the FrameRate that matches the specified Rational.
+     *
+     * @param frameRate
+     *          The Rational frame rate to check for.
+     *
+     * @return
+     *          The matching FrameRate, or null if no FrameRate match the given
+     *          Rational.
+     */
+    public static FrameRate getFrameRate(final Rational frameRate) {
+        if (FrameRate.FPS30.getFrameRate().equals(frameRate)) {
+            return FrameRate.FPS30;
+        }
+
+        if (FrameRate.FPS60.getFrameRate().equals(frameRate)) {
+            return FrameRate.FPS60;
+        }
+
+        return null;
+    }
 }
