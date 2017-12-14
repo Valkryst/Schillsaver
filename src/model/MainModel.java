933f4849918015cad0efe5308e7ca74d5a6d5747
Valkryst <valkryst@valkryst.com>
2017-12-14 12:16:35 -0400

    Implements decode functionality and fixes up a few things.

--- src/model/MainModel.java
+++ src/model/MainModel.java
@@ -73,6 +73,18 @@ public class MainModel extends Model {
         }
     }
 
+    /**
+     * Prepares each of the encoding jobs with their own thread.
+     *
+     * @param settings
+     *          The settings.
+     *
+     * @param view
+     *          The view.
+     *
+     * @return
+     *          The threads.
+     */
     public List<Thread> prepareEncodingJobs(final Settings settings, final MainView view) {
         final List<Thread> encodingJobs = new ArrayList<>();
 
@@ -99,8 +111,6 @@ public class MainModel extends Model {
                     final StringBuilder sb = new StringBuilder();
                     final Formatter formatter = new Formatter(sb, Locale.US);
 
-                    System.out.println(inputFile.getAbsolutePath());
-                    System.out.println(outputFile.getAbsolutePath());
                     formatter.format("\"%s\" -f rawvideo -pix_fmt monob -s %dx%d -r %d -i \"%s\" -vf \"scale=iw*%d:-1\" -sws_flags neighbor -c:v %s -threads 8 -loglevel %s -y \"%s\"",
                                         settings.getStringSetting("FFMPEG Executable Path"),
                                         frameDimension.getWidth() / blockSize.width,
@@ -112,7 +122,7 @@ public class MainModel extends Model {
                                         "verbose",
                                         outputFile.getAbsoluteFile());
 
-                    Platform.runLater(() -> ((TextArea) tab.getContent()).appendText(System.lineSeparator() + System.lineSeparator() + sb.toString()));
+                    Platform.runLater(() -> ((TextArea) tab.getContent()).appendText(sb.toString()));
 
                     // Construct FFMPEG Process:
                     final ProcessBuilder builder = new ProcessBuilder(sb.toString());
@@ -137,9 +147,11 @@ public class MainModel extends Model {
                         logger.error(e);
 
                         final TextArea outputArea = ((TextArea) tab.getContent());
-                        outputArea.appendText("Error:");
-                        outputArea.appendText("\n\t" + e.getMessage());
-                        outputArea.appendText("\n\tSee log file for more information.");
+                        Platform.runLater(() -> {
+                            outputArea.appendText("Error:");
+                            outputArea.appendText("\n\t" + e.getMessage());
+                            outputArea.appendText("\n\tSee log file for more information.");
+                        });
 
                         process.destroy();
                         tab.setClosable(true);
@@ -153,9 +165,11 @@ public class MainModel extends Model {
                     logger.error(e);
 
                     final TextArea outputArea = ((TextArea) tab.getContent());
-                    outputArea.appendText("Error:");
-                    outputArea.appendText("\n\t" + e.getMessage());
-                    outputArea.appendText("\n\tSee log file for more information.");
+                    Platform.runLater(() -> {
+                        outputArea.appendText("Error:");
+                        outputArea.appendText("\n\t" + e.getMessage());
+                        outputArea.appendText("\n\tSee log file for more information.");
+                    });
 
                     tab.setClosable(true);
                     return;
@@ -171,6 +185,18 @@ public class MainModel extends Model {
         return encodingJobs;
     }
 
+    /**
+     * Prepares each of the decoding jobs with their own thread.
+     *
+     * @param settings
+     *          The settings.
+     *
+     * @param view
+     *          The view.
+     *
+     * @return
+     *          The threads.
+     */
     public List<Thread> prepareDecodingJobs(final Settings settings, final MainView view) {
         final List<Thread> decodingJobs = new ArrayList<>();
 
@@ -181,9 +207,84 @@ public class MainModel extends Model {
                 final Thread thread = new Thread(() -> {
                     tab.setClosable(false);
 
-                    System.err.println("DECODE STUFF NOT IMPLEMENTED, MAINMODEL");
+                    // Prepare Files:
+                    final File outputFile = new File(job.getOutputDirectory() + FilenameUtils.removeExtension(inputFile.getName()) + ".zip");
 
-                    inputFile.delete();
+                    if (outputFile.exists()) {
+                        if (outputFile.delete() == false) {
+                            final TextArea outputArea = ((TextArea) tab.getContent());
+                            Platform.runLater(() -> {
+                                outputArea.appendText("Error:");
+                                outputArea.appendText("\n\tUnable to delete " + outputFile.getAbsolutePath());
+                                outputArea.appendText("\n\tTry manually deleting the file, then re-running this decode job.");
+                            });
+
+                            tab.setClosable(true);
+                            return;
+                        }
+                    }
+
+                    // Construct FFMPEG String:
+                    final Dimension blockSize = BlockSize.valueOf(settings.getStringSetting("Encoding Block Size")).getBlockSize();
+
+                    final StringBuilder sb = new StringBuilder();
+                    final Formatter formatter = new Formatter(sb, Locale.US);
+
+                    formatter.format("\"%s\" -i \"%s\" -vf \"format=pix_fmts=monob,scale=iw*%f:-1\" -sws_flags area -loglevel %s -f rawvideo \"%s\"",
+                            settings.getStringSetting("FFMPEG Executable Path"),
+                            inputFile.getAbsolutePath(),
+                            1.0 / blockSize.width,
+                            "verbose",
+                            outputFile.getAbsoluteFile());
+
+                    Platform.runLater(() -> ((TextArea) tab.getContent()).appendText(sb.toString()));
+
+                    // Construct FFMPEG Process:
+                    final ProcessBuilder builder = new ProcessBuilder(sb.toString());
+                    builder.redirectErrorStream(true);
+
+                    Process process = null;
+
+                    try {
+                        process = builder.start();
+                        Runtime.getRuntime().addShutdownHook(new Thread(process::destroy));
+
+                        // Run FFMPEG Process:
+                        final InputStream is = process.getInputStream();
+                        final InputStreamReader isr = new InputStreamReader(is);
+                        final BufferedReader br = new BufferedReader(isr);
+
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            final String temp = line;
+                            Platform.runLater(() -> ((TextArea) tab.getContent()).appendText(System.lineSeparator() + temp));
+                        }
+
+                        br.close();
+                        isr.close();
+                        is.close();
+                    } catch (final IOException e) {
+                        System.out.println(System.currentTimeMillis());
+                        final Logger logger = LogManager.getLogger();
+                        logger.error(e);
+
+                        final TextArea outputArea = ((TextArea) tab.getContent());
+                        Platform.runLater(() -> {
+                            outputArea.appendText("Error:");
+                            outputArea.appendText("\n\t" + e.getMessage());
+                            outputArea.appendText("\n\tSee log file for more information.");
+                        });
+
+                        if (process != null) {
+                            process.destroy();
+                        }
+
+                        tab.setClosable(true);
+                        return;
+                    }
+
+                    Platform.runLater(() -> ((TextArea) tab.getContent()).appendText("\n\nDecoding Complete"));
+
                     tab.setClosable(true);
                 });
 
