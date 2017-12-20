afcbbd45beccd2bfc93455929642c1e029c9faa4
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-12-20 12:33:01 -0400

    Adds check to BlockSize constructor.

--- src/misc/BlockSize.java
+++ src/misc/BlockSize.java
@@ -17,8 +17,15 @@ public enum BlockSize {
      *
      * @param size
      *          The width/height of the block size.
+     *
+     * @throws IllegalArgumentException
+     *          If the size is less than one.
      */
     BlockSize(final int size) {
+        if (size < 1) {
+            throw new IllegalArgumentException("The size cannot be < 1.");
+        }
+
         blockSize = new Dimension(size, size);
     }
 }
